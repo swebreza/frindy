c1709027d900b07409ada9d8f7082c87b3840f4c
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2021-10-24 01:12:46 +0530

    2 resume added

--- webpages/urls.py
+++ webpages/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name='home'),
+    path('post', views.post,name="post"),
+    path('message', views.messages,name="message"),
+    path('profile', views.profile,name="profile"),
+    path('resume', views.resume,name="resume"),
+    path('resume2', views.resume2,name="resume2"),
 ]
--- webpages/views.py
+++ webpages/views.py
@@ -1,5 +1,21 @@
+from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+ return render(request,'webpages/welcome.html')
+
+def messages(request):
  return render(request,'webpages/messages.html')
+
+def profile(request):
+ return render(request,'webpages/profile.html')
+
+def post(request):
+ return render(request,'webpages/post.html')
+
+def resume(request):
+ return render(request,'webpages/resume/resume1.html')
+
+def resume2(request):
+ return render(request,'webpages/resume/resume2.html')
